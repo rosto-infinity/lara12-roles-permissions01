1097042a2d41163459396479d1b7a4537a4546dd
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-08 22:11:06 +0100

    controller role update --role-id 1 --path input --action read --action write

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -6,83 +6,95 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
 
 class RoleController extends Controller
 {
    
     public function index()
     {
-        $users = User::get();
-        return view('role-permission.user.index', ['users' => $users]);
+        $roles = Role::get();
+        return view('role-permission.role.index', ['roles' => $roles]);
     }
 
     public function create()
     {
-        $roles = Role::pluck('name','name')->all();
-        return view('role-permission.user.create', ['roles' => $roles]);
+        return view('role-permission.role.create');
     }
 
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255|unique:users,email',
-            'password' => 'required|string|min:8|max:20',
-            'roles' => 'required'
+            'name' => [
+                'required',
+                'string',
+                'unique:roles,name'
+            ]
         ]);
 
-        $user = User::create([
-                        'name' => $request->name,
-                        'email' => $request->email,
-                        'password' => Hash::make($request->password),
-                    ]);
+        Role::create([
+            'name' => $request->name
+        ]);
 
-        $user->syncRoles($request->roles);
-
-        return redirect('/users')->with('status','User created successfully with roles');
+        return redirect('roles')->with('status','Role Created Successfully');
     }
 
-    public function edit(User $user)
+    public function edit(Role $role)
     {
-        $roles = Role::pluck('name','name')->all();
-        $userRoles = $user->roles->pluck('name','name')->all();
-        return view('role-permission.user.edit', [
-            'user' => $user,
-            'roles' => $roles,
-            'userRoles' => $userRoles
+        return view('role-permission.role.edit',[
+            'role' => $role
         ]);
     }
 
-    public function update(Request $request, User $user)
+    public function update(Request $request, Role $role)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
-            'password' => 'nullable|string|min:8|max:20',
-            'roles' => 'required'
+            'name' => [
+                'required',
+                'string',
+                'unique:roles,name,'.$role->id
+            ]
         ]);
 
-        $data = [
-            'name' => $request->name,
-            'email' => $request->email,
-        ];
+        $role->update([
+            'name' => $request->name
+        ]);
 
-        if(!empty($request->password)){
-            $data += [
-                'password' => Hash::make($request->password),
-            ];
-        }
-
-        $user->update($data);
-        $user->syncRoles($request->roles);
-
-        return redirect('/users')->with('status','User Updated Successfully with roles');
+        return redirect('roles')->with('status','Role Updated Successfully');
     }
 
-    public function destroy($userId)
+    public function destroy($roleId)
     {
-        $user = User::findOrFail($userId);
-        $user->delete();
+        $role = Role::find($roleId);
+        $role->delete();
+        return redirect('roles')->with('status','Role Deleted Successfully');
+    }
 
-        return redirect('/users')->with('status','User Delete Successfully');
+    public function addPermissionToRole($roleId)
+    {
+        $permissions = Permission::get();
+        $role = Role::findOrFail($roleId);
+        $rolePermissions = DB::table('role_has_permissions')
+                                ->where('role_has_permissions.role_id', $role->id)
+                                ->pluck('role_has_permissions.permission_id','role_has_permissions.permission_id')
+                                ->all();
+
+        return view('role-permission.role.add-permissions', [
+            'role' => $role,
+            'permissions' => $permissions,
+            'rolePermissions' => $rolePermissions
+        ]);
+    }
+
+    public function givePermissionToRole(Request $request, $roleId)
+    {
+        $request->validate([
+            'permission' => 'required'
+        ]);
+
+        $role = Role::findOrFail($roleId);
+        $role->syncPermissions($request->permission);
+
+        return redirect()->back()->with('status','Permissions added to role');
     }
 }
---
+++ resources/views/role-permission/role/index.blade.php
@@ -0,0 +1,55 @@
+<x-app-layout>
+  <div class="container mx-auto mt-5">
+    <a href="#" class="bg-blue-500 text-white px-4 py-2 rounded mx-1">Roles</a>
+    <a href="#" class="bg-teal-500 text-white px-4 py-2 rounded mx-1">Permissions</a>
+    <a href="#" class="bg-yellow-500 text-white px-4 py-2 rounded mx-1">Users</a>
+  </div>
+
+  <div class="container mx-auto mt-6">
+    <div class="row">
+      <div class="col-md-12">
+        @if (session('status'))
+      <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+        {{ session('status') }}
+      </div>
+    @endif
+
+        <div class="bg-white shadow-md rounded mt-3">
+          <div class="px-4 py-2 border-b">
+            <h4 class="flex justify-between items-center">
+              Roles
+              <a href="{{ url('roles/create') }}" class="bg-blue-500 text-white px-4 py-2 rounded">Add
+                Role</a>
+            </h4>
+          </div>
+          <div class="p-4">
+
+            <table class="min-w-full border border-gray-300">
+              <thead>
+                <tr>
+                  <th class="border-b">Id</th>
+                  <th class="border-b">Name</th>
+                  <th class="border-b" width="40%">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($permissions as $permission)
+                <tr class="text-center mt-2">
+                    
+                  <td class="border-b py-3">{{ $permission->id }}</td>
+                  <td class="border-b">{{ $permission->name }}</td>
+                  <td class="border-b">
+                    <a href="{{ url('permissions/'.$permission->id. '/edit') }}" class="bg-green-500  text-white my-5 px-2 py-1 rounded">Edit</a>
+                    <a href="{{ url('permissions/'.$permission->id. '/delete') }}" class="bg-red-500 text-white px-2 py-1 rounded mx-2">Delete</a>
+                  </td>
+                  @endforeach
+                </tr>
+              </tbody>
+            </table>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</x-app-layout>
