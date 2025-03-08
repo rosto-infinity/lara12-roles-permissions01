e08136cd4bf3e57a2cc5c1e91f5330b1fe249940
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-08 22:34:13 +0100

    create role

--- resources/views/role-permission/role/create.blade.php
+++ resources/views/role-permission/role/create.blade.php
@@ -14,16 +14,16 @@
         <div class="bg-white shadow-md rounded mt-3">
           <div class="px-4 py-2 border-b">
             <h4 class="flex justify-between items-center">
-              Create Permissions
-              <a href="{{ url('permissions') }}" class="bg-red-500 text-white px-4 py-2 rounded">Back</a>
+              Create Roles
+              <a href="{{ url('roles') }}" class="bg-red-500 text-white px-4 py-2 rounded">Back</a>
             </h4>
           </div>
           <div class="p-4">
-            <form action="{{ url('permissions') }}" method="POST" class="space-y-4">
+            <form action="{{ url('roles') }}" method="POST" class="space-y-4">
               @csrf
 
               <div>
-                <label class="block text-gray-700" for="name">Permission Name</label>
+                <label class="block text-gray-700" for="name">Role Name</label>
                 <input type="text" name="name"
                   class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500" />
               </div>
--- resources/views/role-permission/role/index.blade.php
+++ resources/views/role-permission/role/index.blade.php
@@ -1,11 +1,6 @@
 <x-app-layout>
-  <div class="container mx-auto mt-5">
-    <a href="#" class="bg-blue-500 text-white px-4 py-2 rounded mx-1">Roles</a>
-    <a href="#" class="bg-teal-500 text-white px-4 py-2 rounded mx-1">Permissions</a>
-    <a href="#" class="bg-yellow-500 text-white px-4 py-2 rounded mx-1">Users</a>
-  </div>
-
-  <div class="container mx-auto mt-6">
+ 
+<div class="container mx-auto mt-6">
     <div class="row">
       <div class="col-md-12">
         @if (session('status'))
