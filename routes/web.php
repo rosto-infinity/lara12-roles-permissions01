5e5592095d4203d2d41f6150a5d7e764d6ac380c
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-15 20:44:16 +0100

    resources pour users

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,15 @@ use App\Http\Controllers\UserController;
     Route::resource('permissions',  PermissionController::class);
     Route::get('permissions/{permissionId}/delete',  [PermissionController::class, 'destroy'])->name('permissions.destroy');
     
+    //resources pour roles; permissions
     Route::resource('roles',  RoleController::class);
     Route::get('roles/{roleId}/delete',  [RoleController::class, 'destroy'])->name('roles.destroy');
     Route::get('roles/{roleId}/give-permissions',   [RoleController::class, 'addPermissionToRole'])->name('roles.addPermissionToRole');
     
+   
     Route::put('roles/{roleId}/give-permissions',   [RoleController::class, 'givePermissionToRole'])->name('give.addPermissionToRole');
-    // Route::post('roles/{roleId}/give-permissions',   [RoleController::class, 'storePermissionToRole'])->name('roles.storePermissionToRole');
-    // Route::get('roles/{roleId}/revoke-permissions',   [RoleController::class, 'removePermissionFromRole'])->name('roles.removePermissionFromRole');
-
+    
+    //resources pour users
     Route::resource('users',  UserController::class);
    
 
