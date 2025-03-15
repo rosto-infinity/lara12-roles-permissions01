9aad5b18de19b2b70137b83006df31ca29839f67
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-15 12:25:19 +0100

    success add role

--- resources/views/role-permission/role/add-permissions.blade.php
+++ resources/views/role-permission/role/add-permissions.blade.php
@@ -2,13 +2,11 @@
   <div class="container mx-auto mt-5">
       <div class="row">
           <div class="col-md-12">
-              @if ($errors->any())
-              <ul class="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded relative" role="alert">
-                  @foreach ($errors->all() as $error)
-                      <li>{{ $error }}</li>
-                  @endforeach
-              </ul>
-              @endif
+            @if (session('status'))
+            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+              {{ session('status') }}
+            </div>
+          @endif
               <div class="bg-white shadow-md rounded-lg overflow-hidden">
                   <div class="bg-gray-200 px-4 py-2">
                       <h4 class="font-bold text-lg flex items-center justify-between">
@@ -22,6 +20,9 @@
                           @method('PUT')
 
                           <div class="mb-3">
+                            @error('permission')
+                                <div class="text-red-500">{{ $message }}</div>                               
+                            @enderror
                             <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Permissions</label>
                             <div class="grid grid-cols-2 gap-4">
                                 @foreach ($permissions as $permission)
@@ -30,14 +31,14 @@
                                         <input
                                             type="checkbox"
                                             name="permission[]"
-                                            value="{{ $permission->id }}"
+                                            value="{{ $permission->name}}"
                                             class="form-checkbox h-4 w-4 text-blue-600"
                                             />
 
                                         <span class="ml-2 text-gray-700">
                                             {{ $permission->name }}
                                         </span>
-                                        
+
                                     </label>
                                 </div>
                                 @endforeach
