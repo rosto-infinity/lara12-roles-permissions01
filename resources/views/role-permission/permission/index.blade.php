812a0af2784b630f4c8512633a278eaebcba1927
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-15 21:03:23 +0100

    Ajout de commentaires pour clarifier le code dans la vue des permissions

--- resources/views/role-permission/permission/index.blade.php
+++ resources/views/role-permission/permission/index.blade.php
@@ -1,51 +1,55 @@
 <x-app-layout>
   
-@include('role-permission.nav-links')
-  <div class="container mx-auto mt-6">
-    <div class="row">
-      <div class="col-md-12">
-        @if (session('status'))
-      <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-        {{ session('status') }}
-      </div>
-    @endif
-
-        <div class="bg-white shadow-md rounded mt-3">
-          <div class="px-4 py-2 border-b bg-teal-200">
-            <h4 class="flex justify-between items-center">
-              Permissions
-              <a href="{{ url('permissions/create') }}" class="bg-teal-500 text-white px-4 py-2 rounded">Add
-                Permission</a>
-            </h4>
-          </div>
-          <div class="p-4">
-
-            <table class="min-w-full border border-gray-300">
-              <thead>
-                <tr>
-                  <th class="border-b">Id</th>
-                  <th class="border-b">Name</th>
-                  <th class="border-b" width="40%">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach ($permissions as $index => $permission)
-                <tr class="text-center mt-2 {{ $index % 2 == 0 ? 'bg-gray-100' : 'bg-white' }}">
-                    <td class="border-b py-3">{{ $permission->id }}</td>
-                    <td class="border-b">{{ $permission->name }}</td>
-                    <td class="border-b">
-                        <a href="{{ url('permissions/'.$permission->id. '/edit') }}" class="bg-green-500 text-white my-5 px-2 py-1 rounded">Edit</a>
-                        <a href="{{ url('permissions/'.$permission->id. '/delete') }}" class="bg-red-500 text-white px-2 py-1 rounded mx-2">Delete</a>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-            
-            </table>
-
+  @include('role-permission.nav-links')
+    <div class="container mx-auto mt-6">
+      <div class="row">
+        <div class="col-md-12">
+          {{-- Affichage du message de statut, le cas échéant --}}
+          @if (session('status'))
+            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+              {{ session('status') }}
+            </div>
+          @endif
+  
+          <div class="bg-white shadow-md rounded mt-3">
+            <div class="px-4 py-2 border-b bg-teal-200">
+              <h4 class="flex justify-between items-center">
+                Permissions
+                {{-- Lien pour ajouter une nouvelle permission --}}
+                <a href="{{ url('permissions/create') }}" class="bg-teal-500 text-white px-4 py-2 rounded">Add Permission</a>
+              </h4>
+            </div>
+            <div class="p-4">
+  
+              {{-- Tableau des permissions --}}
+              <table class="min-w-full border border-gray-300">
+                <thead>
+                  <tr>
+                    <th class="border-b">Id</th>
+                    <th class="border-b">Name</th>
+                    <th class="border-b" width="40%">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {{-- Boucle pour afficher chaque permission --}}
+                  @foreach ($permissions as $index => $permission)
+                  <tr class="text-center mt-2 {{ $index % 2 == 0 ? 'bg-gray-100' : 'bg-white' }}">
+                      <td class="border-b py-3">{{ $permission->id }}</td>
+                      <td class="border-b">{{ $permission->name }}</td>
+                      <td class="border-b">
+                          {{-- Lien pour éditer la permission --}}
+                          <a href="{{ url('permissions/'.$permission->id. '/edit') }}" class="bg-green-500 text-white my-5 px-2 py-1 rounded">Edit</a>
+                          {{-- Lien pour supprimer la permission --}}
+                          <a href="{{ url('permissions/'.$permission->id. '/delete') }}" class="bg-red-500 text-white px-2 py-1 rounded mx-2">Delete</a>
+                      </td>
+                  </tr>
+                  @endforeach
+                </tbody>
+              </table>
+  
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</x-app-layout>
+  </x-app-layout>
