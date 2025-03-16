dc5fe747c67128c1b12329bef0b6a943546edaa2
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-16 20:08:55 +0100

    Ajout de vérifications d'autorisation pour les liens de création et de gestion des permissions des rôles dans la vue des rôles

--- resources/views/role-permission/role/index.blade.php
+++ resources/views/role-permission/role/index.blade.php
@@ -14,7 +14,9 @@
             <h4 class="flex justify-between items-center">
               <span class="text-2xl text-black">Roles</span>
               {{-- Lien pour ajouter un nouveau rôle --}}
+              @can('create role')
               <a href="{{ url('roles/create') }}" class="bg-blue-500 text-white px-4 py-2 rounded">Add Role</a>
+              @endcan
             </h4>
           </div>
           <div class="p-4">
@@ -37,17 +39,18 @@
             <td class="border-b">
 
             {{-- Lien pour ajouter/éditer les permissions du rôle --}}
-            @can('update addPermissionToRole')
+            @can('give-permissions role')
               <a href="{{ url('roles/' . $role->id . '/give-permissions') }}"
                 class="bg-orange-400 text-white my-5 px-2 py-1.5 rounded">
                 Add / Edit Role Permission
               </a>
-           @endcan
-
+              @endcan
+         
+            
             {{-- Lien pour éditer le rôle --}}
             @can('update role')
               <a href="{{ url('roles/' . $role->id . '/edit') }}"
-                class="bg-green-500 text-white my-5 px-2 py-1.5 rounded">
+                class="bg-green-500 text-white ml-3 my-5 px-2 py-1.5 rounded">
                 Edit
               </a>
            @endcan
