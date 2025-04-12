30665325a43e21faaad90870e515a78f06138203
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-12 20:53:28 +0100

    * 3Display the user's profile form.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,7 +12,7 @@ use Illuminate\View\View;
 class ProfileController extends Controller
 {
     /**
-     * Display the user's profile form.
+     * 3Display the user's profile form.
      */
     public function edit(Request $request): View
     {
