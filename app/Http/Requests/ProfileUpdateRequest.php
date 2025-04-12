04354c60a916f8df5b2d3e61adebd53b19db4982
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-12 21:03:47 +0100

    * 32Get the validation rules that apply to the request.

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -9,7 +9,7 @@ use Illuminate\Validation\Rule;
 class ProfileUpdateRequest extends FormRequest
 {
     /**
-     * Get the validation rules that apply to the request.
+     * 32Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
