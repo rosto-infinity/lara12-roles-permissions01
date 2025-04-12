ad49ae62595e955a247432173fb0ac487824c39e
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-12 21:01:03 +0100

    24Handle an incoming request.

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 class Admin
 {
   /**
-   * Handle an incoming request.
+   * 24Handle an incoming request.
    *
    * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
    */
